bd05fa8ae6afbeef3f3ce0d78721de269d1666a1
unknown <jetbrains@WINDOWS-UP6VA3T.labs.intellij.net>
2015-05-26 18:59:55 +0400

    added platform code generator

---
+++ generatePlatformHeader.bat
@@ -0,0 +1,12 @@
+SetLocal EnableDelayedExpansion
+systeminfo | findstr /B /C:"OS Configuration" > osConfiguration.txt
+FOR /F "tokens=4" %%F IN (osConfiguration.txt) DO (
+SET var=%%F
+)
+ECHO %var%
+if "%var%" == "Server" ( 
+echo #define _WIN_SERVER_VAR_ TRUE > include2.h
+) ELSE (
+echo #define _WIN_SERVER_VAR_ FALSE > include2.h
+)
+del /q osConfiguration.txt
---
+++ include2.h
@@ -0,0 +1,1 @@
+#define _WIN_SERVER_VAR_ TRUE 
